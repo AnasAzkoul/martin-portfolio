987e44a8c9e2111512d82d083eb6a444cfab9571
Anas azkoul <anas@Anass-MacBook-Air.local>
2023-02-10 14:09:19 +0100

    a bug fix in the Navlinks

--- src/components/header/NavLinks.tsx
+++ src/components/header/NavLinks.tsx
@@ -1,4 +1,6 @@
 import React from 'react'; 
+import {useContext} from 'react';
+import { uiContext } from '@/context/UIContext';
 import Link from 'next/link';
 import { navLinks } from '../../utils/constants'; 
 import styles from './navbar.module.css'; 
@@ -8,13 +10,14 @@ type Props = {
 }
 
 const NavLinks = ({type}: Props) => {
+  const {closeSidebar} = useContext(uiContext); 
   return (
     <ul
       className={`${styles.links} ${type === 'mobile' && styles.links_mobile}`}
     >
       {navLinks.map((link) => {
         return (
-          <li key={link.id}>
+          <li key={link.id} onClick={closeSidebar}>
             <Link href={link.url} className='w-full'>{link.text}</Link>
           </li>
         );
