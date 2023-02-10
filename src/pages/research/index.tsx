a33d09e2a12497e672f16aeb1757f72fe0fae2e7
Anas azkoul <anas@Anass-MacBook-Air.local>
2023-02-10 14:04:08 +0100

    finalized the publications section in the research page

--- src/pages/research/index.tsx
+++ src/pages/research/index.tsx
@@ -9,26 +9,38 @@ const research = () => {
   return (
     <>
       <Layout>
-        <h2 className='text-4xl text-secondary font-semibold'>My Publications</h2>
-        <div className='md:w-4/5'>
-          <Paragraph>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-            aspernatur accusamus nemo molestias fuga et at quae mollitia provident
-            magni.
-          </Paragraph>
+        <div className={styles.container}>
+          <h2 className='text-4xl text-secondary font-semibold'>
+            My Publications
+          </h2>
+          <div className='md:w-4/5'>
+            <Paragraph>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+              aspernatur accusamus nemo molestias fuga et at quae mollitia
+              provident magni.
+            </Paragraph>
+          </div>
+          <ul className={styles.publications}>
+            {publicationsData.map((item) => {
+              return (
+                <li
+                  key={item.id}
+                  className={styles.publication}
+                >
+                  <CgChevronRight className='text-inherit' size={20}/>
+                  <a
+                    href={item.href}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    className='ml-4 text-inherit'
+                  >
+                    {item.title}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
         </div>
-        <ul>
-          {publicationsData.map((item) => {
-            return (
-              <li key={item.id} className='flex items-center py-4'>
-                <CgChevronRight />
-                <a href={item.href} target='_blank' rel='noopener noreferrer' className='ml-4 text-xl text-gray-500'>
-                  {item.title}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
       </Layout>
     </>
   );
