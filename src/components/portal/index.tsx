789df2a2cebdfe55ac65019c8941ca2250b46e10
Anas azkoul <anas@Anass-MacBook-Air.local>
2023-02-23 17:48:37 +0100

    more styling fixes and created programmatic portal

---
+++ src/components/portal/index.tsx
@@ -0,0 +1,43 @@
+import React, {useState, useEffect} from 'react'; 
+import {createPortal} from 'react-dom'; 
+
+const createWrapperAndAppendToBody = (wrapperID: string) => {
+  if (!document) return; 
+  const wrapperElement = document.createElement('div'); 
+  wrapperElement.setAttribute('id', wrapperID); 
+  document.body.appendChild(wrapperElement); 
+  return wrapperElement; 
+}
+
+type Props = {
+  children: React.ReactElement; 
+  wrapperID: string
+}
+
+const Portal = ({children, wrapperID}: Props) => {
+  
+  const [wrapperElement, setWrapperElement] = useState<HTMLElement>(); 
+  
+  useEffect(() => {
+    let element = document.getElementById(wrapperID) as HTMLDivElement | undefined; 
+    let systemCreated = false; 
+    
+    if(!element) {
+      systemCreated = true; 
+      element = createWrapperAndAppendToBody(wrapperID); 
+    }
+    setWrapperElement(element); 
+    
+    return () => {
+      if (systemCreated && element?.parentNode) {
+        element.parentNode.removeChild(element); 
+      }
+    }
+  }, [wrapperID])
+  
+  if (!wrapperElement) return null; 
+  
+  return createPortal(children, wrapperElement); 
+}
+
+export default Portal
