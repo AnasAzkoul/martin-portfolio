f270ce36e2631a588cbafb3c1d73cd64418e5f4e
Anas azkoul <anas@Anass-MacBook-Air.local>
2023-03-17 10:50:34 +0100

    finalized the photos on the second article

--- src/pages/research/[slug].tsx
+++ src/pages/research/[slug].tsx
@@ -1,22 +1,20 @@
-import React, {useContext, useEffect, useRef} from 'react';
-import Layout from '@/components/Layout/Layout';
-import ArticleLayout from '@/components/Articles/ArticleLayoutMDX';
-import Canvas from '@/components/Canvas/Canvas';
-import type { GetStaticProps, GetStaticPaths } from 'next';
+import React, { useContext, useEffect, useRef } from "react";
+import Layout from "@/components/Layout/Layout";
+import ArticleLayout from "@/components/Articles/ArticleLayoutMDX";
+import Canvas from "@/components/Canvas/Canvas";
+import type { GetStaticProps, GetStaticPaths } from "next";
 import {
   getArticleData,
   getNextAndPrevArticle,
   articlesFiles,
-} from '../../lib/helpers/getArticles';
-import { serialize } from 'next-mdx-remote/serialize';
-import {MDXRemote, MDXRemoteSerializeResult} from 'next-mdx-remote';
-import { useScrollProgressBar } from '@/lib/hooks/useScrollProgressbar';
-import styles from '../../styles/Research.module.css';
-import Image from 'next/image';
-import ArticleImage from '@/components/ui/ArticleImage';
-import NextArticleLink from '@/components/ui/ArticleLinkButton';
-
-
+} from "../../lib/helpers/getArticles";
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
+import { useScrollProgressBar } from "@/lib/hooks/useScrollProgressbar";
+import styles from "../../styles/Research.module.css";
+import Image from "next/image";
+import ArticleImage from "@/components/Articles/ArticleImage";
+import ArticleLinkButton from "@/components/ui/ArticleLinkButton";
 
 type Props = {
   articleData: {
@@ -28,27 +26,31 @@ type Props = {
     >;
   };
 
-  nextPage: string
-  prevPage: string
+  nextPage: string;
+  prevPage: string;
 };
 
 const components = { Canvas, ArticleImage, Image };
 
-
-
-const ArticlePage = ({articleData, nextPage, prevPage}: Props) => {
+const ArticlePage = ({ articleData, nextPage, prevPage }: Props) => {
   const progressRef = useScrollProgressBar();
 
   return (
     <Layout>
-      <div id='progress' className={styles.progress} ref={progressRef}></div>
+      <div id="progress" className={styles.progress} ref={progressRef}></div>
       <ArticleLayout title={articleData.title}>
         <MDXRemote {...articleData.mdxSource} components={components} />
         <div className={`flex justify-between relative pt-4 px-4`}>
           {prevPage && (
-            <NextArticleLink href={`/research/${prevPage}`} direction='left'>Prev</NextArticleLink>
+            <ArticleLinkButton href={`/research/${prevPage}`} direction="left">
+              Prev
+            </ArticleLinkButton>
           )}
-          {nextPage && <NextArticleLink href={`/research/${nextPage}`} direction='right'>Next</NextArticleLink>}
+          {nextPage && (
+            <ArticleLinkButton href={`/research/${nextPage}`} direction="right">
+              Next
+            </ArticleLinkButton>
+          )}
         </div>
       </ArticleLayout>
     </Layout>
@@ -62,14 +64,13 @@ export const getStaticProps: GetStaticProps = async (context) => {
 
   const articleData = await getArticleData(slug as string, serialize);
 
-  const {nextPage, prevPage} = await getNextAndPrevArticle(slug as string);
-
+  const { nextPage, prevPage } = await getNextAndPrevArticle(slug as string);
 
   return {
     props: {
       articleData,
       nextPage: !nextPage ? null : nextPage,
-      prevPage: !prevPage ? null : prevPage
+      prevPage: !prevPage ? null : prevPage,
     },
   };
 };
@@ -79,7 +80,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   const slugs = getAllArticles.map((article) => {
     return {
-      params: { slug: article.replace(/\.mdx$/, '') },
+      params: { slug: article.replace(/\.mdx$/, "") },
     };
   });
 
