b1031560d105d61e85daf186f54abd0687eb9903
Anas azkoul <anas@Anass-MacBook-Air.local>
2023-03-22 13:50:49 +0100

    added favicons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,25 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicons/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
         <meta
           name="description"
           content="Martin Drozdik a freelance software developer and researcher based in Vienna."
